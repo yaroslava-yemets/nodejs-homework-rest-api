68dd8c1a8b0e7824199f44a684d5e680efaaf2a3
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2021-11-23 00:16:58 +0200

    deleted extra checking

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,14 +1,7 @@
-const { Unauthorized } = require('http-errors')
-
 const { User } = require('../../models')
 
 const current = async(req, res) => {
-  const { _id, token } = req.user
-  console.log(_id)
-  console.log(token)
-  if (!token) {
-    throw new Unauthorized('You are not authorized')
-  }
+  const { _id } = req.user
   const { email, subscription } = await User.findById(_id)
   res.json({
     status: 'success',
