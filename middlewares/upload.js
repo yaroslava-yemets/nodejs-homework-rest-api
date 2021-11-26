0a7262e45b15f274fa06732bc05256cc77b1078b
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2021-11-27 00:50:53 +0200

    added examine of mimetype for image

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,5 +1,6 @@
 const multer = require('multer')
 const path = require('path')
+const { BadRequest } = require('http-errors')
 
 const tempDir = path.join(__dirname, '../', 'tmp')
 console.log(tempDir)
@@ -16,8 +17,25 @@ const uploadConfig = multer.diskStorage({
   }
 })
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/webp' ||
+    file.mimetype === 'image/svg+xml' ||
+    file.mimetype === 'image/gif' ||
+    file.mimetype === 'image/tiff'
+  ) {
+    cb(null, true)
+  } else {
+    cb(new BadRequest('You can upload only image type'), false)
+  }
+}
+
 const upload = multer({
-  storage: uploadConfig
+  storage: uploadConfig,
+  fileFilter
 })
 
 module.exports = upload
