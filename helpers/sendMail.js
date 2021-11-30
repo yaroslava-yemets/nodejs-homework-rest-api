73c861b4a809b9e100bdea6eb28165c9e8c51f8a
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2021-12-01 00:03:35 +0200

    deleted extra console

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -6,7 +6,6 @@ sgMail.setApiKey(SENDGRID_API_KEY)
 const sendMail = async(data) => {
   const email = { ...data, from: 'yaroslava1058@gmail.com' }
   await sgMail.send(email)
-  console.log('we sent the letter')
   return true
 }
 
